4d01e84cfd158025db883380a41bfad57b128859
Bent Brüggemann <bent.brueggemann@blackbit.de>
2024-06-12 22:47:13 +0200

    tests: added tests for from_file trait of configs

--- src/env/traits.rs
+++ src/env/traits.rs
@@ -8,6 +8,56 @@ pub trait FromFile {
     {
         let content = fs::read_to_string(file_path)?;
         let config: Self = toml::from_str(&content)?;
+
         Ok(config)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::env::temp_dir;
+    use super::*;
+    use std::fs::File;
+    use std::io::Write;
+    use serde::Deserialize;
+
+    #[derive(Deserialize)]
+    struct TestConfig {
+        key: String,
+    }
+
+    impl FromFile for TestConfig {}
+
+    #[test]
+    fn test_from_file_valid_toml() {
+        let dir = temp_dir();
+        let file_path = dir.as_path().join("test_trait1.toml");
+        let mut file = File::create(&file_path).unwrap();
+
+        writeln!(file, r#"key = "value""#).unwrap();
+
+        let config = TestConfig::from_file(file_path.to_str().unwrap()).unwrap();
+
+        assert_eq!(config.key, "value");
+    }
+
+    #[test]
+    fn test_from_file_invalid_toml() {
+        let dir = temp_dir();
+        let file_path = dir.as_path().join("test_trait2.toml");
+        let mut file = File::create(&file_path).unwrap();
+
+        writeln!(file, r#"keyv = "value""#).unwrap();
+
+        let result = TestConfig::from_file(file_path.to_str().unwrap());
+
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_from_file_non_existent_file() {
+        let result = TestConfig::from_file("non_existent_file.toml");
+
+        assert!(result.is_err());
     }
 }
